117d6f489799960c4f355459f3ad8da55cd0ff53
Ray Gonçalves <raaycunha.dev@gmail.com>
2026-07-26 02:23:14 -0300

    feat: adiciona estilo, responsividade e alguns estilos

--- 9_projects_react_ts/github-finder/src/pages/UserRepo.tsx
+++ 9_projects_react_ts/github-finder/src/pages/UserRepo.tsx
@@ -1,6 +1,6 @@
-import { CodeXml, CornerRightDown } from "lucide-react";
+import { CodeXml, GitFork, Star } from "lucide-react";
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useFinderContext } from "../contexts/useFinderContext";
 import type { UserRepository } from "../types/User";
 
@@ -21,28 +21,46 @@ const UserRepo = () => {
     navigate("/github-finder", { replace: true });
   };
   return (
-    <div>
-      <h2>
-        Explore os repositórios do usuário: {username}
-        <CornerRightDown size={15} />
+    <div className="flex flex-col items-center gap-8">
+      <h2 className="text-md font-semibold md:text-2xl">
+        Explore os repositórios do(a) {username}
       </h2>
       {repositories.length > 0 ? (
-        <div>
+        <div className="flex flex-col flex-wrap gap-6 items-left md:flex-row">
           {repositories.map((repo: UserRepository) => (
-            <div key={repo.id}>
-              <span>Nome do projeto: {repo.name}</span>
-              <span>
+            <div
+              key={repo.id}
+              className="flex flex-col gap-2 bg-[#0d1117] p-4 rounded-lg"
+            >
+              <span className="font-semibold">{repo.name}</span>
+              <div className="flex items-center gap-1">
                 <CodeXml size={20} />
-                {repo.language}
-              </span>
-              <div>
-                <span>{repo.stargazers_count}</span>
-                <span>{repo.forks_count}</span>
+                <span>{repo.language}</span>
               </div>
-              <Link to={repo.html_url}>Ver código</Link>
+              <div className="flex gap-6">
+                <div className="flex gap-1">
+                  <Star className="text-yellow-500" size={25} />
+                  <span>{repo.stargazers_count}</span>
+                </div>
+                <div className="flex gap-1">
+                  <GitFork className="text-blue-500" size={25} />
+                  <span>{repo.forks_count}</span>
+                </div>
+              </div>
+              <a
+                className="bg-green-600 w-25 text-white font-bold p-2 mt-2 rounded-lg cursor-pointer transition-all duration-400 hover:scale-[0.99] hover:bg-green-700"
+                href={repo.html_url}
+              >
+                Ver código
+              </a>
             </div>
           ))}
-          <button onClick={handleSearch}>Buscar outro usuário</button>
+          <button
+            onClick={handleSearch}
+            className="bg-cyan-700 w-25 text-white font-bold p-2 mt-2 rounded-lg cursor-pointer transition-all duration-400 hover:scale-[0.99] hover:bg-green-700"
+          >
+            Buscar outro usuário
+          </button>
         </div>
       ) : (
         <div>
